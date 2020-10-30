15d20eaa3c5799162740ff309ce4aac1708f6840
wangyihua <18639003229@163.com>
2020-10-30 16:03:39 +0800

    update bash

--- merge.sh
+++ merge.sh
@@ -1,4 +1,7 @@
 name=$1
+GREEN_COLOR='\E[1;32m' #绿
+RED_COLOR='\E[1;31m'  #红
+YELOW_COLOR='\E[1;33m' #黄
 br=`git branch | grep "*"`
 array=('dev')
 if [ $name == 'test' ]
@@ -13,6 +16,7 @@ then
 fi
 len=${#array[@]}
 for ((i=0;i<$len;i++));do
+    echo -e  "${GREEN_COLOR}======正在merge${${array[$i]}}分支======"
     git checkout ${array[$i]}
     git pull
     git merge ${br/* /}
@@ -20,8 +24,10 @@ for ((i=0;i<$len;i++));do
     then 
     git commit
     git push
+    echo -e  "${GREEN_COLOR}======${${array[$i]}}分支merge成功======"
     else
-    echo '请解决冲突后切回开发分支重新merge'
+    echo -e  "${RED_COLOR}======请处理冲突后切回原分支重新merge======"
     fi
 done
 git checkout ${br/* /}
+echo -e  "${YELOW_COLOR}======bash执行结束======"
